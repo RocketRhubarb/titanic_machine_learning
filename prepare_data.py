ca5ad835756862e17e04d28f212a210e68f0c647
Andreas Röckert <mr.rockert@gmail.com>
2018-02-14 15:20:11 +0100

    Minor changes

--- prepare_data.py
+++ prepare_data.py
@@ -1,5 +1,45 @@
 import pandas as pd
 
-df_train = pd.read_csv('data/train.csv')
+'''
+Loading comma separated files into pandas dataframes
+'''
+class load_data(self):
+    def __init__(self):
+        
+    def columns(self, columns):
+        self.columns = columns 
+    
+    
+    
+def df_train():
+    '''
+    Load and modifying training set
+    '''
+    df = pd.read_csv('data/train.csv')
+    df.set_index('PassengerId',inplace = True)
+    
+    # Remove Name
+    #df = df[['Survived', 'Pclass','Sex',
+    #         'Age', 'SibSp', 'Parch',
+    #         'Ticket','Fare', 'Cabin', 'Embarked']]
+    
+    # Modify Embarked
+    emb_dict = {'S':0, 'C':1, 'Q':2} 
+    df.replace({"Embarked": emb_dict},inplace=True)
+    return df
 
-print(df_train)
+def df_test():
+    '''
+    Load test set
+    '''
+    df = pd.read_csv('data/test.csv')
+    df.set_index('PassengerId',inplace = True)
+    return df
+
+def df_gender_submission():
+    '''
+    Example of submission file
+    '''
+    df = pd.read_csv('data/gender_submission.csv')
+    df.set_index('PassengerId',inplace = True)
+    return df
